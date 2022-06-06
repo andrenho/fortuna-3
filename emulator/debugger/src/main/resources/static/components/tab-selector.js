c2984cfbaf8ff440cc45d86fe6d9969e081b6f47
Andre Wagner <andre.nho@gmail.com>
2022-06-06 15:19:47 -0500

    wip

--- emulator/debugger/src/main/resources/static/components/tab-selector.js
+++ emulator/debugger/src/main/resources/static/components/tab-selector.js
@@ -1,10 +1,18 @@
 /*
 
 Attributes:
-  - tabs: a comma-separated list of tags. Each tag will be both the title, and the ID of the element
-          that should appear when the tab is selected.
+  - tabs: a comma-separated list of tags. Each tag will be both the title.
   - selected: the current selected index
 
+Events:
+  - index-change: when the index is changed
+
+Usage:
+    <tab-selector tabs="Debugger,SD Card,Documentation" selected="0" id="tab-selector"></tab-selector>
+
+    document.getElementById("tab-selector").addEventListener("index-change", (e) => {
+        document.getElementById("my-element").style.visibility = (e.detail === 0) ? "visible" : "hidden";
+    });
  */
 
 window.customElements.define("tab-selector", class extends HTMLElement {
@@ -34,23 +42,31 @@ window.customElements.define("tab-selector", class extends HTMLElement {
                 padding: 6px 14px;
                 cursor: pointer;
                 background-color: lightgray;
+                font-weight: bold;
+                box-shadow: 6px 6px 6px lightgray;
             }
 
             .tab-middle {
                 border-bottom: 1px black solid;
-                width: 12px;
+                width: 24px;
             }
 
             .tab-selected {
                 background: white;
                 border-bottom: 0;
             }
+            
+            .white-box {
+                background: white;
+                position: relative;
+                height: 10px;
+                z-index: 1;
+            }
         </style>
         
-        <div class="tabs" id="tabs">
-            <div class="tab-start"></div>
-            <div class="tab-end" id="tab-end"></div>
-        </div>
+        <div class="tabs" id="tabs"></div>
+        <div class="white-box"></div>
+        <slot></slot>
     `;
 
     #tabNames = [];
@@ -73,29 +89,34 @@ window.customElements.define("tab-selector", class extends HTMLElement {
         } else if (prop === "selected") {
             this.#selected = parseInt(newValue);
             this.#update();
+            this.dispatchEvent(new CustomEvent("index-change", { detail: this.#selected }));
         }
     }
 
     #update() {
-        const tabs = this.shadowRoot.querySelector("#tabs");
-        const tabEnd = this.shadowRoot.querySelector("#tab-end");
+        const createTab = className => {
+            const div = document.createElement("div");
+            div.className = className;
+            return div;
+        };
 
-        const tabMiddle = document.createElement("div");
-        tabMiddle.className = "tab-middle";
+        const tabElements = [createTab("tab-start")];
 
-        tabs.innerHTML = "";
         for (let i = 0; i < this.#tabNames.length; ++i) {
-            const tab = document.createElement("div");
-            tab.className = "tab";
-            if (i === this.#selected)
+            const tab = createTab("tab");
+            if (i === this.#selected) {
                 tab.classList.add("tab-selected");
+            }
             tab.innerText = this.#tabNames[i];
             tab.addEventListener("click", () => {
                 this.setAttribute("selected", i.toString());
             });
-            tabs.insertBefore(tab, tabEnd);
+            tabElements.push(tab);
 
-            tabs.insertBefore(tabMiddle.cloneNode(), tabEnd);
+            tabElements.push(createTab("tab-middle"));
         }
+        tabElements.push(createTab("tab-end"));
+
+        this.shadowRoot.querySelector("#tabs").replaceChildren(...tabElements);
     }
 });
