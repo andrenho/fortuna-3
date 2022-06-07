13cabb4af61c8ae149c7df2ffaa726b9e1960f9b
Andre Wagner <andre.nho@gmail.com>
2022-06-07 09:44:52 -0500

    wip

--- emulator/debugger/src/main/java/com/fortuna3/fortuna3/Fortuna3Application.java
+++ emulator/debugger/src/main/java/com/fortuna3/fortuna3/Fortuna3Application.java
@@ -1,7 +1,10 @@
 package com.fortuna3.fortuna3;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Fortuna3Application {
---
+++ emulator/debugger/src/main/resources/static/js/debugger.js
@@ -0,0 +1,15 @@
+const $ = (id) => document.querySelector(id);
+
+window.addEventListener("load", () => {
+
+    //
+    // TAB MANAGEMENT
+    //
+
+    $("#tab-selector").addEventListener("index-change", (e) => {
+        $("#debugger").style.display = (e.detail === 0) ? "flex" : "none";
+        $("#sd-card").style.display = (e.detail === 1) ? "flex" : "none";
+        $("#documentation").style.display = (e.detail === 2) ? "flex" : "none";
+    });
+
+});
