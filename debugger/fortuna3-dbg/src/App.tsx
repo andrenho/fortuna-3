eb508a4bda1a758167baa9fa96c4f013f72c52dc
Andre Wagner <andre.nho@gmail.com>
2022-06-17 14:01:46 -0500

    wip

---
+++ debugger/fortuna3-dbg/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {hello} from "fortuna3-emu";
+
+function App() {
+
+  console.log(hello());
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
