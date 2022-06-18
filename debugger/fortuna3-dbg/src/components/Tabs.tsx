056c66038d95256835456c49fbcb2bcfe94137e5
Andre Wagner <andre.nho@gmail.com>
2022-06-17 19:58:21 -0500

    wip

--- debugger/fortuna3-dbg/src/components/Tabs.tsx
+++ debugger/fortuna3-dbg/src/components/Tabs.tsx
@@ -1,5 +1,19 @@
-export function Tab() {
+import CSS from 'csstype';
+
+const tabs : CSS.Properties = {
+    display: "flex",
+    flexDirection: "row",
+    whiteSpace: "nowrap"
 }
 
-export default function Tabs() {
+function Tab() : JSX.Element {
+    return <div></div>;
 }
+
+function Item() : JSX.Element {
+    return <div></div>;
+}
+
+Tab.Item = Item;
+
+export default Tab;
