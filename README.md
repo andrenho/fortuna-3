bf7f9086bda1db1a5db3fd7fd55faecc3eaa6c58
Andre Wagner <andre.nho@gmail.com>
2022-06-25 13:27:49 -0500

    wip

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fortuna-3
 A homemade Z80 computer.
+
+To build the debugger:
+
+```
+cd debugger
+./build.sh
+```
---
+++ debugger/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+if ! command -v emcc &> /dev/null; then
+  echo "emcc is required"
+  exit
+fi
+
+if ! command -v npm &> /dev/null; then
+  echo "npm is required"
+  exit
+fi
+
+echo "Building WASM emulator..."
+cd fortuna3-emu/src-wasm && make && cd ../..
+
+echo "Building TS emulator..."
+cd fortuna3-emu && npm install && npm run build && cd ..
+
+echo "Building debugger..."
+cd fortuna3-dbg && npm install && cd ..
+
+echo "All done. Cd into fotuna3-dbg and run 'npm start' to run the debugger."
