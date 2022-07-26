d4bb3b136224c310b52dcdf28f292467f3150d48
André Wagner <andre.nho@gmail.com>
2022-07-26 11:25:17 -0300

    .

--- old2/emulator/frontend/components/common.js
+++
@@ -1,3 +0,0 @@
-hex = (value, pad=2, withPrefix=false) => (withPrefix ? "0x" : "") + value.toString(16).padStart(pad, '0');
-
-toWords = (bytes) => Uint16Array.from(Array.from({ length: bytes.length / 2 }, (_, i) => bytes[i*2] + (bytes[(i*2)+1] * 256)));
