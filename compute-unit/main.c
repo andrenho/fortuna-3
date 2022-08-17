1b62777fc074b8fea12105155dfe6c09d5d1c1e5
André Wagner <andre.nho@gmail.com>
2022-08-17 18:57:34 -0300

    .

--- compute-unit/main.c
+++ compute-unit/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 #include <util/delay.h>
@@ -12,63 +13,57 @@
 #include "uart.h"
 #include "z80.h"
 
-typedef enum {
-    EV_NONE, EV_MONITOR, EV_USR1, EV_IORQ, EV_REMOTE,
-} Event;
-volatile Event last_event = EV_NONE;
+typedef struct {
+    bool monitor : 1;
+    bool usr     : 1;
+    bool iorq    : 1;
+    bool remote  : 1;
+} Events;
+
+volatile Events events = { false, false, false, false };
 
 int main(void)
 {
     initialize();
     puts_P(PSTR("Welcome to Fortuna-3!\n"));
 
-    sei();
+    z80_reset();
 
 #if INCLUDE_MONITOR && RUN_MONITOR_AT_START
-    z80_reset();
     cli();
     monitor();
-    sei();
 #endif
+
+    sei();
 
     while (1) {
-        switch (last_event) {
-
-            case EV_NONE:
-                break;
-
-            case EV_MONITOR:
-                cli();
-                last_event = EV_NONE;
-                _delay_ms(80);
-                sei();
-                break;
-
-            case EV_USR1:
-                cli();
+        if (events.monitor) {
+            cli();
+            events.monitor = false;
 #if INCLUDE_MONITOR
-                monitor();
+            monitor();
 #endif
-                last_event = EV_NONE;
-                _delay_ms(80);
-                sei();
-                break;
-
-            case EV_IORQ:
-                cli();
-                z80_iorq();
-                last_event = EV_NONE;
-                sei();
-                break;
-
-            case EV_REMOTE:
-                cli();
-                remote();
-                last_event = EV_NONE;
-                sei();
-                break;
+            _delay_ms(80);
+            sei();
         }
 
+        if (events.usr) {
+            // TODO
+        }
+
+        if (events.iorq) {
+            cli();
+            z80_iorq();
+            events.iorq = false;
+            sei();
+        }
+
+        if (events.remote) {
+            cli();
+            remote();
+            events.remote = false;
+            sei();
+        }
     }
 
     return 0;
@@ -81,17 +76,17 @@ ISR(BADISR_vect)
 
 ISR(INT2_vect)
 {
-    last_event = EV_MONITOR;
+    events.monitor = true;
 }
 
 ISR(INT3_vect)
 {
-    last_event = EV_USR1;
+    events.usr = true;
 }
 
 ISR(INT4_vect)
 {
-    last_event = EV_IORQ;
+    events.iorq = true;
 }
 
 static uint8_t latest_char = 0;
@@ -99,7 +94,7 @@ ISR(USART0_RX_vect)
 {
     uint8_t udr = UDR0;
     if (latest_char == 0xfe && udr == 0xf0)
-        last_event = EV_REMOTE;
+        events.remote = true;
     latest_char = udr;
 }
 
