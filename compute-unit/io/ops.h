cad1e27ea6db64670494ef712b4c5fd5899e2c0c
André Wagner <andre.nho@gmail.com>
2022-08-22 21:56:25 -0300

    .

--- compute-unit/io/ops.h
+++ compute-unit/io/ops.h
@@ -6,22 +6,21 @@
 #define S_GET_BLK 0x11
 #define S_PRINT_Z 0x11
 #define S_PRINT_LEN 0x12
-
-#define RTC_SET 0x15
-#define RTC_GET_DATE 0x16
-#define RTC_GET_TIME 0x17
-
+#define RTC_SET 0x13
+#define RTC_GET_YEAR 0x12
+#define RTC_GET_MONTH 0x13
+#define RTC_GET_DAY 0x14
+#define RTC_GET_HOUR 0x15
+#define RTC_GET_MINUTES 0x16
+#define RTC_GET_SECONDS 0x17
 #define LCD_LINE1 0x18
 #define LCD_LINE2 0x19
 #define LCD_CLEAR 0x1A
 #define LCD_CHAR 0x1B
 #define LCD_CMD 0x1C
-
 #define RANDOM 0x1D
-
 #define SD_READ 0x1E
 #define SD_WRITE 0x1F
-
 #define FS_MOUNT 0x20
 #define FS_OPEN_R 0x21
 #define FS_OPEN_W 0x22
@@ -40,7 +39,6 @@
 #define FS_READDIR 0x2D
 #define FS_FREE 0x2E
 #define FS_FORMAT 0x2F
-
 #define MM_BANK 0x40
 #define MM_CPY 0x41
 #define MM_CPY_FAR 0x42
@@ -54,7 +52,6 @@
 #define MM_TO_DEC 0x46
 #define MM_TO_HEX 0x47
 #define MM_TO_BIN 0x48
-
 #define SUM 0x50
 #define SUBTRACT 0x51
 #define MULTIPLY 0x52
@@ -62,7 +59,6 @@
 #define MODULO 0x54
 #define COMPARE_16 0x55
 #define COMPARE_32 0x56
-
 #define TIMER0_SET 0x60
 #define TIMER1_SET 0x61
 #define TIMER0_GET 0x62
