093a2376c0045ed229e4c048fac9031dca3fba9e
André Wagner <andre.nho@gmail.com>
2022-08-23 17:31:49 -0300

    .

--- compute-unit/io/iomemory.c
+++ compute-unit/io/iomemory.c
@@ -1,12 +1,39 @@
 #include "io/iomemory.h"
 
+#include <stddef.h>
+
+#include "dev/ram.h"
+
 void io_mm_cpy(IO_Regs* r)
 {
-    uint8_t block[256];
+    // TODO - copy full blocks to improve performance
+
+    uint16_t addr_from = Pa(r);
+    uint16_t addr_to = Pb(r);
+    uint16_t sz = Qa(r);
+
+    for (uint16_t i = 0; i < sz; ++i) {
+        uint8_t data = ram_get_byte(addr_from + i);
+        ram_set_byte(addr_to + i, data);
+    }
 }
 
 void io_mm_cpy_far(IO_Regs* r)
 {
+    // TODO - copy full blocks to improve performance
+
+    uint16_t addr_from = Pa(r);
+    uint16_t addr_to = Pb(r);
+    uint8_t bank_from = r->Qb0;
+    uint8_t bank_to = r->Qb1;
+    uint16_t sz = Qa(r);
+
+    for (uint16_t i = 0; i < sz; ++i) {
+        ram_set_bank(bank_from);
+        uint8_t data = ram_get_byte(addr_from + i);
+        ram_set_bank(bank_to);
+        ram_set_byte(addr_to + i, data);
+    }
 }
 
 void io_mm_strcpy(IO_Regs* r)
