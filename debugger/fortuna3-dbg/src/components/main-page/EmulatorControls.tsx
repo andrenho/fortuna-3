53a4b9e2619e6b64d73b255e33a8b5c484f7d781
Andre Wagner <andre.nho@gmail.com>
2022-09-17 15:25:19 -0500

    wip

--- debugger/fortuna3-dbg/src/components/main-page/EmulatorControls.tsx
+++ debugger/fortuna3-dbg/src/components/main-page/EmulatorControls.tsx
@@ -13,13 +13,13 @@ const EmulatorControls : React.FC = () => {
             store.reset();
     }
 
-    return (<div className={css.buttons}>
+    return <div className={css.buttons}>
         <button title="Run" onClick={() => store.run()} className={css.button}>&#x25B6;</button>
         <button title="Step one instruction" onClick={() => store.step()} className={css.button} style={{letterSpacing: "-4px",}}>&#x25B6;||</button>
         <button title="Step one screenful" onClick={() => store.stepOneScreenful()} className={css.button}>&#x1f4fa;</button>
         <Spacing />
         <button title="Reset emulator" onClick={onReset} className={css.button} style={{transform: "scaleX(-1)", fontSize: "24px"}}>&#x21B4;</button>
-    </div>);
+    </div>;
 };
 
 export default EmulatorControls;
