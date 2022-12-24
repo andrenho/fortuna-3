a6455e64ca572aef095391cef8806ade56392b40
Andre Wagner <andre.nho@gmail.com>
2022-12-24 09:04:09 -0600

    .

--- remote-server/remote-server.py
+++ remote-server/remote-server.py
@@ -5,27 +5,57 @@ import socket
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 
+import serial
+import sys
+
 PORT = 8026
+serial_port = ''
+
+
+class FortunaSerial:
+
+    @staticmethod
+    def connect():
+        return serial.Serial(serial_port, baudrate = 1000000, timeout = 0.5)
+
+    @staticmethod
+    def disconnect(ser):
+        ser.close()
+
+    @staticmethod
+    def check_response(ser):
+        c = ser.read(1)
+        if (c[0] != 0):
+            raise Exception("Error communicating with Fortuna-3")
+
+    def reset(self):
+        ser = self.connect()
+        try:
+            ser.write(b"\xfe\xf0\xff")
+            self.check_response(ser)
+        finally:
+            self.disconnect(ser)
 
 
 class RemoteServer(BaseHTTPRequestHandler):
 
+    fserial = FortunaSerial()
+
     def reset(self):
+        self.fserial.reset()
         return b'Reset successful.'
 
     def upload_bios(self, data):
-        print(data)
-        return b'BIOS upload successful.'
+        raise Exception('Not implemented yet.')
 
     def upload_firmware(self):
-        return b'Firmware upload successful.'
+        raise Exception('Not implemented yet.')
 
     def format_sdcard(self):
-        raise Exception("Error formatting SDCard")
+        raise Exception('Not implemented yet.')
 
     def upload_file(self, filename, data):
-        print(filename, data)
-        return b'File upload successful.'
+        raise Exception('Not implemented yet.')
 
     def get_request_data(self):
         content_length = int(self.headers['Content-Length'])
@@ -60,6 +90,11 @@ class RemoteServer(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: ' + sys.argv[0] + ' SERIAL_PORT', file=sys.stderr)
+        sys.exit(1)
+    serial_port = sys.argv[1]
+
     logging.basicConfig(level=logging.INFO)
     httpd = HTTPServer(('', PORT), RemoteServer)
     hostname = socket.gethostname()
