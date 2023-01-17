234716a60d8c59897ec0518d3a2a0b9a2262be50
Andre Wagner <andre.nho@gmail.com>
2023-01-17 11:32:08 -0600

    WIP

--- debugger-backend/src/main/java/com/fortuna3/compiler/controller/CompilerRestController.java
+++ debugger-backend/src/main/java/com/fortuna3/compiler/controller/CompilerRestController.java
@@ -5,7 +5,6 @@ import com.fortuna3.output.dto.DebuggingInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,13 +14,12 @@ public class CompilerRestController {
     private final CompilerService compilerService;
 
     @GetMapping("/compiler/crc")
-    public ResponseEntity<Integer> getCrc(@RequestParam(value="collapse_macros", defaultValue="false") Boolean collapseMacros) {
+    public ResponseEntity<Integer> getCrc() {
         return ResponseEntity.ok(compilerService.getHash());
     }
 
     @GetMapping("/compiler/output")
-    public ResponseEntity<DebuggingInfo> getDebuggingInfo(@RequestParam(value="collapse_macros", defaultValue="false") Boolean collapseMacros) {
-        compilerService.setCollapseMacros(collapseMacros);
+    public ResponseEntity<DebuggingInfo> getDebuggingInfo() {
         return ResponseEntity.ok(compilerService.getCurrentDebuggingInfo());
     }
 
---
+++ debugger-backend/src/main/java/com/fortuna3/options/controller/OptionsRestController.java
@@ -0,0 +1,28 @@
+package com.fortuna3.options.controller;
+
+import com.fortuna3.options.dto.Options;
+import com.fortuna3.options.service.OptionsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OptionsRestController {
+
+    private final OptionsService optionsService;
+
+    @GetMapping("/options")
+    public ResponseEntity<Options> getOptions() {
+        return ResponseEntity.ok(optionsService.getOptions());
+    }
+
+    @PutMapping("/options")
+    public ResponseEntity<Void> putOptions(@RequestBody Options options) {
+        optionsService.updateOptions(options);
+        return ResponseEntity.ok().build();
+    }
+}
