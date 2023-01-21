fc0a3a7c80a480ee9b39d13eed149d304034c7f9
Andre Wagner <andre.nho@gmail.com>
2023-01-21 05:53:19 -0600

    WIP

--- fortuna-pi/src/main.c
+++ fortuna-pi/src/main.c
@@ -1,72 +1,71 @@
 #include <SDL.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
-    /*
-    SDL_Init( 0 );
+    (void) argc; (void) argv;
+
+    SDL_Init(0);
 
     printf("Testing video drivers...\n");
+    bool drivers[SDL_GetNumVideoDrivers()];
     for (int i = 0; i < SDL_GetNumVideoDrivers(); ++i)
     {
         drivers[i] = (0 == SDL_VideoInit(SDL_GetVideoDriver(i)));
         SDL_VideoQuit();
     }
 
-    std::cout << "SDL_VIDEODRIVER available:";
-    for( int i = 0; i < drivers.size(); ++i )
-    {
-        std::cout << " " << SDL_GetVideoDriver( i );
+    printf("SDL_VIDEODRIVER available:");
+    for (int i = 0; i < SDL_GetNumVideoDrivers(); ++i) {
+        printf(" %s", SDL_GetVideoDriver(i));
     }
-    std::cout << '\n';
+    puts("");
 
-    std::cout << "SDL_VIDEODRIVER usable   :";
-    for( int i = 0; i < drivers.size(); ++i )
-    {
-        if( !drivers[ i ] ) continue;
-        std::cout << " " << SDL_GetVideoDriver( i );
+    printf("SDL_VIDEODRIVER usable:");
+    for (int i = 0; i < SDL_GetNumVideoDrivers(); ++i) {
+        if(!drivers[i])
+            continue;
+        printf(" %s", SDL_GetVideoDriver(i));
     }
-    std::cout << '\n';
+    puts("");
 
-    if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
-        std::cerr << "SDL_Init(): " << SDL_GetError() << '\n';
+        fprintf(stderr, "SDL_Init(): %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
-    std::cout << "SDL_VIDEODRIVER selected : " << SDL_GetCurrentVideoDriver() << '\n';
+    printf("SDL_VIDEODRIVER selected: %s\n", SDL_GetCurrentVideoDriver());
 
-    SDL_Window* window = SDL_CreateWindow
-            (
-                    "SDL2",
-                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                    640, 480,
-                    SDL_WINDOW_SHOWN
-            );
-    if( nullptr == window )
-    {
-        std::cerr << "SDL_CreateWindow(): " << SDL_GetError() << '\n';
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        640, 480,
+        SDL_WINDOW_SHOWN
+    );
+    if (!window) {
+        fprintf(stderr, "SDL_CreateWindow(): %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
 
-    std::cout << "SDL_RENDER_DRIVER available:";
-    for( int i = 0; i < SDL_GetNumRenderDrivers(); ++i )
-    {
+    printf("SDL_RENDER_DRIVER available: ");
+    for (int i = 0; i < SDL_GetNumRenderDrivers(); ++i) {
         SDL_RendererInfo info;
-        SDL_GetRenderDriverInfo( i, &info );
-        std::cout << " " << info.name;
+        SDL_GetRenderDriverInfo(i, &info);
+        printf("%s ", info.name);
     }
-    std::cout << '\n';
+    puts("");
 
-    SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
-    if( nullptr == renderer )
-    {
-        std::cerr << "SDL_CreateRenderer(): " << SDL_GetError() << '\n';
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "SDL_CreateRenderer(): %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
+
     SDL_RendererInfo info;
     SDL_GetRendererInfo( renderer, &info );
-    std::cout << "SDL_RENDER_DRIVER selected : " << info.name << '\n';
+    printf("SDL_RENDER_DRIVER selected: %s\n", info.name);
 
     bool running = true;
     unsigned char i = 0;
@@ -82,15 +81,16 @@ int main(int argc, char* argv[])
             }
         }
 
-        SDL_SetRenderDrawColor( renderer, i, i, i, SDL_ALPHA_OPAQUE );
+        SDL_SetRenderDrawColor( renderer, i % 255, ((i + 64) * 2) % 255, ((i + 128) / 2) % 255, SDL_ALPHA_OPAQUE );
         SDL_RenderClear( renderer );
         SDL_RenderPresent( renderer );
+        SDL_Delay(16);
         i++;
     }
 
     SDL_DestroyRenderer( renderer );
     SDL_DestroyWindow( window );
     SDL_Quit();
-     */
+
     return 0;
 }
